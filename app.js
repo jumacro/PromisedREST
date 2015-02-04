0fed5ae062fb345fdb578ba8701510dc47b849e6
Mithun Das <mithun.programmer@gmail.com>
2015-02-05 00:05:13 +0530

    app.js reformed to support resources

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express             = require('express')
+    ,   path            = require('path')
+    ,   logger          = require('morgan')
+    ,   cookieParser    = require('cookie-parser')
+    ,   bodyParser      = require('body-parser');
+var passport = require('passport');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -20,41 +14,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// declaring the router file
+var router = require('./app/routers');
 
-// error handlers
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /v1
+app.use('/v1', router);
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
+//expose app to other modules
 module.exports = app;
