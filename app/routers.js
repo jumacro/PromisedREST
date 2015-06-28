00a7b19238f9f7a1d4988f714a5e12773a783540
Mithun Das <mithun.programmer@gmail.com>
2015-06-28 18:53:07 +0530

    redining the routers

--- app/routers.js
+++ app/routers.js
@@ -10,13 +10,9 @@ var express     = require('express'),
     Device      = require('./Controller/DevicesController'),
     Block       = require('./Controller/UserBlocksController'),
     Report       = require('./Controller/ReportsController'),
-    Post        = require('./Controller/PostsController'),
-    PostResponse = require('./Controller/PostResponsesController'),
-    Photo       = require('./Controller/PhotosController'),
     Channel     = require('./Controller/ChannelsController'),
     Message     = require('./Controller/MessagesController'),
-    Event     = require('./Controller/EventsController'),
-    queryParser = require('./Component/QueryParser'),
+    QueryParser = require('./Component/QueryParser'),
     multiparty = require('connect-multiparty'),
     multipartyMiddleware = multiparty(),
     app         = express();
@@ -51,12 +47,6 @@ router.route('/users/:id')
 router.route('/users/facebook/:facebookId')
   .get(Auth.isAuthenticated, User.getUserByFbId);
 
-router.route('/users/:userId/feeds')
-  .get(Auth.isAuthenticated, User.getFeeds);
-
-router.route('/users/facebook/:facebookId/feeds')
-  .get(Auth.isAuthenticated, User.getFeedsByFbId);
-
 router.route('/users/:userId/devices')
   .post(Auth.isAuthenticated, Device.registerDevice);
 
@@ -90,80 +80,8 @@ router.route('/users/:userId/post/:postId/blocks/reports')
   .delete(Auth.isAuthenticated, Report.removePostReport);
 
 
-
-/** POST/DIPS ROUTES **/
-
-router.route('/posts')
-  .get(Auth.isAuthenticated, Post.getAllPosts)
-  .put(Auth.isAuthenticated, Post.updateAllPosts);
-
-
-router.route('/posts/:id')
-  .get(Auth.isAuthenticated, Post.getPost)
-//  .put(Auth.isAuthenticated, Post.updatePost)
-  .delete(Auth.isAuthenticated, Post.deletePost);
-
-router.route('/users/:userId/posts')
-  .post(Auth.isAuthenticated, multipartyMiddleware, Post.postUserPosts)
-  .get(Auth.isAuthenticated, Post.getUserPosts)
-  .delete(Auth.isAuthenticated, Post.deleteUserPosts); 
-
-router.route('/users/:userId/posts/active')
-  .get(Auth.isAuthenticated, Post.getUserActivePost); 
-
-//router.route('/posts/:id/deactivate')
-//  .put(Auth.isAuthenticated, Post.expirePost); 
-
-/** Admin routes **/
-
-router.route('/admin/posts')
-  .get(Auth.isAuthenticated, Post.getPosts);
-
-/** RESPONSE ROUTES **/
-
-
-router.route('/responses')
-  .delete(Auth.isAuthenticated, PostResponse.deleteAll);
-
-router.route('/users/:userId/responses')
-  .get(Auth.isAuthenticated, PostResponse.getMyResponses);
-
-router.route('/users/:userId/responses/read')
-  .put(Auth.isAuthenticated, PostResponse.markAllAsRead);
-
 router.route('/users/:userId/messages')
   .get(Auth.isAuthenticated, Channel.getMyMessages);
-
-router.route('/responses/:id')
-  .get(Auth.isAuthenticated, PostResponse.responseById)
-  .put(Auth.isAuthenticated, PostResponse.updateResponse);
-
-router.route('/posts/:postId/responses')
-  .post(Auth.isAuthenticated, PostResponse.addResponse)
-  .get(Auth.isAuthenticated, PostResponse.getResponses)
-  .delete(Auth.isAuthenticated, PostResponse.deleteResponses);
-
-router.route('/posts/:postId/responses/read')
-  .put(Auth.isAuthenticated, PostResponse.markAsRead);
-
-router.route('/posts/:postId/responses/:id')
-  .delete(Auth.isAuthenticated, PostResponse.deleteAResponse);
-
-
-/** PHOTO ROUTES **/
-
-router.route('/users/:userId/photos')
-  .post(Auth.isAuthenticated, Photo.postUserProfilePhoto)
-  .get(Auth.isAuthenticated, Photo.getUserPhotos)
-  .delete(Auth.isAuthenticated, Photo.deleteUserPhotos); 
-
-//do not use it for production now
-router.route('/users/:userId/photos/bulk')
-  .post(Auth.isAuthenticated, Photo.postUserProfilePhotoBulk);
-
-router.route('/users/:userId/posts/:postId/photos')
-  .post(Auth.isAuthenticated, Photo.postUserPostPhoto)
-  .get(Auth.isAuthenticated, Photo.getUserPostPhotos);
 
 
 /****** CHANNELS AND MESSAGES ********************/
@@ -190,32 +108,8 @@ router.route('/channels/:channelId/:userId/messages')
 
 /*****************************************************/
 
-
-/***** SPECIAL ROUTES **********************/
-
-//notifiers
-//router.route('/users/:userId/feeds/:created')
-//  .get(Auth.isAuthenticated, Feeds.notify)
-
-router.route('/users/:userId/events') // takes created as query param if needed
-  .get(Auth.isAuthenticated, Event.notify);
-
 router.route('/devices/test') // takes created as query param if needed
   .get(Auth.isAuthenticated, Device.testDevice);
-
-
-
-/******  ADMIN ROUTES ***********/
-// Only for Admin use --- Login
-router.route('/admin/login')                  
-  .post(Auth.isAuthenticated, User.login);
-// Only for Admin use --- Login
-router.route('/admin/logout')
-  .get(Auth.isAuthenticated, User.logout);
-
-
-
-
 
 
 /** Expose router to other modules **/
