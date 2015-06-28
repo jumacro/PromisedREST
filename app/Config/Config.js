924d6abc0bde031a915f210291f88496d663f87f
Mithun Das <mithun.programmer@gmail.com>
2015-06-28 18:50:24 +0530

    deleted loose ends

--- app/Config/Config.js
+++ app/Config/Config.js
@@ -6,13 +6,13 @@ module.exports = {
             dbName: 'fondoo'
         },
         remote: {
-            host: 'apollo.modulusmongo.net',
+            host: 'test',
             port: 27017,
-            dbName: 'zopomU2s',
+            dbName: 'test',
             options: {
                 //TODO change credentials
-                username: 'mitZXtrm',
-                password: 'GodKcalB'
+                username: 'test',
+                password: 'test'
             }
         }
     },
@@ -22,40 +22,26 @@ module.exports = {
             encrypt_key: '4b 8?((~FKnpD))>8kb!B |#-uXIO24G3rc:&MG+FR{x;r#Uq4k{Ef@F4E9^-qS!', //change hash key
         },
         api: {
-            appId: 'fondoo-rest', 
-            appSecret: 'pDblTMZaFam59d@F9c#V1G9UEL17)Odz', 
+            appId: 'test-resty', 
+            appSecret: 'pAoQWEiuy1!@', 
         },
         tokenLife: 3600
     },
-    mailer: {
-        auth: {
-         user: 'test@example.com',
-         pass: 'secret',
-       },
-       defaultFromAddress: 'First Last <test@examle.com>'
+    gcm: {
+        apiKey: "AIzaSyBRH3MvZtxUA9ggmhpIMIvuE37s8f1d7Yk"
     },
-    cloudinary: {
-        //setttings for the multer options
-        cloud_name: 'virgin-labs-inc', 
-        api_key: '877858668556557', 
-        api_secret: '9uRNZ-JFL79ApRgHnyBLQrCQwq0' 
-    },
-    "gcm": {
-        "apiKey": "AIzaSyBRH3MvZtxUA9ggmhpIMIvuE37s8f1d7Yk"
-    },
-
-    "apn": {
-        "connection": {
-            "gateway": "gateway.push.apple.com",
-            "cert": "./cert/pushcert.pem",
-            "key": "./cert/pushcert.pem"
+    apn: {
+        connection: {
+            gateway: "gateway.push.apple.com",
+            cert: "./cert/pushcert.pem",
+            key: "./cert/pushcert.pem"
         },
-        "feedback": {
-            "address": "feedback.push.apple.com",
-            "cert": "./cert/pushcert.pem",
-            "key": "./cert/pushcert.pem",
-            "interval": 43200,
-            "batchFeedback": true
+        feedback: {
+            address: "feedback.push.apple.com",
+            cert: "./cert/pushcert.pem",
+            key: "./cert/pushcert.pem",
+            interval: 43200,
+            batchFeedback: true
         }
     },
     debug         : false,
