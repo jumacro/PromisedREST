9ac7192987955a7a30963a1988a10e70406acecf
Mithun Das <mithun.das@jumacro.com>
2018-10-25 15:45:24 +0000

    Update gitignore

---
+++ env/development.js
@@ -0,0 +1,9 @@
+// 
+const devConfig = {
+    env: 'development',
+    MONGOOSE_DEBUG: true,
+    db: 'mongodb+srv://mithun:DymX1CL70yEUX3M7@cluster0-jx21f.gcp.mongodb.net/test?retryWrites=true',
+    port: 8000
+};
+
+export default devConfig;
